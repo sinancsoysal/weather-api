92f7cdd837c6a8f7668b62e13d651ef7a4b48650
Sinan Can Soysal <ssoysal@thomasmurray.com>
2023-05-30 15:08:58 +0100

    add ui

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask,jsonify,url_for,request
+from flask import Flask, jsonify, render_template, request
 import requests
 from flask_restful import Resource,Api
 app=Flask(__name__)
 api=Api(app)
 
-@app.route('/weather',methods=['POST'])
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/weather',methods=['POST'], endpoint='weather')
 def result():
     city_name = request.form['city']
     geocoding = "https://geocoding-api.open-meteo.com/v1/search?name={}&count=10&language=en&format=json"
@@ -14,9 +20,9 @@ def result():
         lat = coordinations["results"][0]["latitude"]
         long = coordinations["results"][0]["longitude"]
         result = requests.get(weather.format(lat, long)).json()
-        return jsonify(result)
+        return render_template('results.html', result=result['hourly']['temperature_2m'][0], city=city_name)
     except:
-        return "<h1>Oops city not found. Try larger city name nearby</h1>"
+        return render_template('results.html', result=None)
 
 if __name__ == '__main__':
     app.run()
